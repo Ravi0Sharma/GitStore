09c0d65cdaca1833cf9a8026dff001a15c468ae6
Ravi Sharma <ravisha@chalmers.se>
2025-12-11 00:24:57 +0100

    #3 fix the commit head

--- internal/core/log.go
+++ internal/core/log.go
@@ -3,7 +3,7 @@ package core
 // Log returns commits in reverse chronological order (from HEAD backwards).
 func (repo *Repo) Log() []*Commit {
 	var history []*Commit
-	commit := repo.HEAD
+	commit := repo.HEAD.Head
 
 	for commit != nil {
 		history = append(history, commit)
