17efba31abe8e85b7001ce0cf6a7d1b7346c4c84
Ravi Sharma <ravisha@chalmers.se>
2025-12-11 01:01:16 +0100

    fix null error

--- cmd/gitclone/main.go
+++ cmd/gitclone/main.go
@@ -6,12 +6,26 @@ import (
 	"gitclone/internal/core"
 )
 
+func printLog(label string, commits []*core.Commit) {
+	fmt.Println("==", label, "==")
+	for _, c := range commits {
+		fmt.Println(c.ID, c.Message)
+	}
+}
+
 func main() {
 	repo := core.NewRepo("test")
 
-	c1 := repo.Commit("Initial commit")
-	c2 := repo.Commit("Another commit")
+	repo.Commit("Initial commit")
+	repo.Commit("Change 1")
 
-	fmt.Println(c1.ID, c1.Message) // Should print: 0 Initial commit
-	fmt.Println(c2.ID, c2.Message) // Should print: 1 Another commit
+	printLog("master log", repo.Log()) // 1, 0
+
+	repo.Checkout("testing")
+	repo.Commit("Change 2 on testing")
+
+	printLog("testing log", repo.Log()) // 2,1,0
+
+	repo.Checkout("master")
+	printLog("master log again", repo.Log()) // fortfarande 1,0
 }
