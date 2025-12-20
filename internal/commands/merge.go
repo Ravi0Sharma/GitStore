70766a6eb1a900b66bbf1c18db41fb453fa646db
Ravi Sharma <ravisha@chalmers.se>
2025-12-20 15:39:06 +0100

    #30 Correct comment to make it more readable

--- internal/commands/merge.go
+++ internal/commands/merge.go
@@ -2,10 +2,9 @@ package commands
 
 import (
 	"fmt"
+	"gitclone/internal/storage"
 	"os"
 	"time"
-
-	"gitclone/internal/storage"
 )
 
 func Merge(args []string) {
@@ -46,7 +45,7 @@ func Merge(args []string) {
 		return
 	}
 
-	// Read tips of both branches
+	// Read commits of both branches
 	currentTip, err := storage.ReadHeadRefMaybe(cwd, writeOptions, currentBranch)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -105,4 +104,3 @@ func Merge(args []string) {
 
 	fmt.Printf("[%s %d] %s\n", currentBranch, mergeID, mergeMessage)
 }
-
