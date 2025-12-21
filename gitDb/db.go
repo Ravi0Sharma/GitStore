4773577ac83490ee88a7bd7df85393ac73eb0f37
Ravi Sharma <ravisha@chalmers.se>
2025-12-21 19:24:19 +0100

    #34 Add create db instance

--- gitDb/db.go
+++ gitDb/db.go
@@ -1,1 +1,16 @@
 package GitDb
+
+import "fmt"
+
+type DB struct {
+	log   []byte
+	index *Index
+}
+
+// Open initializes a new database instance
+func Open(_ string) (*DB, error) {
+	return &DB{
+		log:   make([]byte, 0, 4096),
+		index: newIndex(),
+	}, nil
+}
