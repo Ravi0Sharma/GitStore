384c0aa1ef1a3604fc06dc3365b7698aa4d3783c
Ravi Sharma <ravisha@chalmers.se>
2025-12-25 01:34:58 +0100

    #40 Add sign upp page ui

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/landingPage/landingPage"
 import LoginPage from "./pages/loginPage"
+import SignUpPage from "./pages/signUp"
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/SignIn" element={<LoginPage />} />
+      <Route path="/SignUp" element={<SignUpPage />} />
     </Routes>
   )
 }
